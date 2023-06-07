17e7b03212e3d5624fb6750865fe501a25a08023
Milos Cubrilo <milos.cubrilo@gmail.com>
2023-06-07 09:08:20 +0200

    Free dataset memory after use in ministat
    This doesn't matter in original ministat as the process exits
    immediately after code which dynamically allocates memory runs, but it
    can matter in minibench depending on the way it is used

--- ministat.h
+++ ministat.h
@@ -7,6 +7,7 @@
  * ----------------------------------------------------------------------------
  *
  * mcubrilo: repurposed as a C header and removed some parts
+ * mcubrilo: free memory before returning from ministat
  *
  */
 
@@ -506,4 +507,11 @@ ministat(int ci, int nds, int n_samples, double data[][n_samples], const char* n
 		if (!flag_n)
 			Relative(ds[i], ds[0], ci);
 	}
+
+	for (i = 0; i < nds; i++) {
+		free(ds[i]->name);
+		free(ds[i]->points);
+		free(ds[i]);
+	}
+
 }
